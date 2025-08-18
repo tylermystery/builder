c3d2a8618917362f3b5617de0041747a4aceebc2
tylermystery <tyler@tylersmysterytours.com>
2025-08-17 17:02:26 -0700

    Update state.js

--- state.js
+++ state.js
@@ -1,15 +1,17 @@
 /*
- * Version: 1.0.0
+ * Version: 1.2.0
  * Last Modified: 2025-08-17
  *
  * Changelog:
+ *
+ * v1.2.0 - 2025-08-17
+ * - Added `currentSort` property to the UI state for unified sorting.
  *
  * v1.0.0 - 2025-08-17
  * - Initial versioning and changelog added.
  */
 
 export const state = {
-
     records: {
         all: [],
         filtered: [],
@@ -30,6 +32,7 @@ export const state = {
     ui: {
         recordsCurrentlyDisplayed: 0,
         isLoadingMore: false,
+        currentSort: 'reactions-desc', // Default sort method
     },
     history: {
         undoStack: [],
