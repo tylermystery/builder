17e9bddaf17184751cc4bafd0574e4faaa265f16
tylermystery <tyler@tylersmysterytours.com>
2025-08-23 21:12:31 -0700

    Update api.js

--- api.js
+++ api.js
@@ -8,11 +8,14 @@
  * - Implemented dynamic collage generation for Grouping items via Cloudinary.
  * - Added text-based acronyms as a fallback for options without images.
  * - fetchImagesForRecord is now hierarchy-aware.
+/*
+ * This module is responsible for all network requests to external services (Airtable, Cloudinary).
+ * It should not import from main.js. It can import from utility files like utils.js or session.js.
  */
 import { state } from './state.js';
 import { CONSTANTS, CLOUDINARY_CLOUD_NAME } from './config.js';
 import { storeSession } from './session.js';
-import { parseOptions } from './ui.js';
+import { parseOptions } from './utils.js';
 
 const PERSONAL_ACCESS_TOKEN = 'patI1bum8NZvXmYV5.9961c676b00f5e5a9f006c6c26d1ba93ecde2b489f419a68d2a1cb43ff781c57';
 const BASE_ID = 'app5yTznb3R5YNUFw';
