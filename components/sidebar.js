0f578c06ce6c9c089eeab4736718b5400a0a1bc3
tylermystery <tyler@tylersmysterytours.com>
2025-09-01 17:15:07 -0700

    Update sidebar.js

--- components/sidebar.js
+++ components/sidebar.js
@@ -2,7 +2,7 @@ import { state } from '../state.js';
 import * as ui from '../ui.js';
 import * as api from '../api.js';
 import { CONSTANTS } from '../config.js';
-import { parseOptions } from '../utils/utils.js';
+import { parseOptions } from '../utils.js'; // REPAIRED: Corrected import path
 import { log } from '../utils/debug.js';
 
 async function createFavoriteCardElement(record, itemInfo, imageCache) {
