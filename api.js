196e67c66fef316169723b627ceb7e8425789ad7
tylermystery <tyler@tylersmysterytours.com>
2025-09-01 17:03:41 -0700

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import { state } from './state.js';
 import { CONSTANTS, CLOUDINARY_CLOUD_NAME } from './config.js';
 import { storeSession } from './session.js';
-import { parseOptions } from './utils/utils.js'; // REPAIRED: Corrected the import path
+import { parseOptions } from './utils.js'; // REPAIRED: Corrected the import path
 
 const PERSONAL_ACCESS_TOKEN = 'patI1bum8NZvXmYV5.9961c676b00f5e5a9f006c6c26d1ba93ecde2b489f419a68d2a1cb43ff781c57';
 const BASE_ID = 'app5yTznb3R5YNUFw';
@@ -232,3 +232,4 @@ export async function fetchImagesForRecord(record, allRecords, imageCache) {
     imageCache.set(cacheKey, result);
     return result;
 }
+
