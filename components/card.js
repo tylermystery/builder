d180c3377aa8af41378e0c6dd9d604b4aeaf02df
tylermystery <tyler@tylersmysterytours.com>
2025-09-16 19:35:37 -0700

    Update card.js

--- components/card.js
+++ components/card.js
@@ -1,4 +1,4 @@
-// FILE: components/card.js
+// PASTE THIS ENTIRE CODE INTO: components/card.js
 import { state } from '../state.js';
 import * as ui from '../ui.js';
 import { CONSTANTS, CLOUDINARY_CLOUD_NAME } from '../config.js';
