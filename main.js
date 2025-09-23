90b066b0571d140c1397ac018e3d214ed2a3bb82
tylermystery <tyler@tylersmysterytours.com>
2025-09-23 11:24:35 -0700

    Update main.js

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ async function initialize() {
         state.ui.activeShopId = activeShop.id;
         const titleElement = document.getElementById('main-shop-title');
         // This single line sets the title and the link correctly
-        titleElement.innerHTML = `<a href="/?shopId=${activeShop.id}" class="shop-title-link">${activeShop.fields.Name} Shop</a><button id="shop-switcher-trigger" style="background:none; border:none; color:transparent; cursor:pointer; font-size: 1em; vertical-align: super;">s</button>`;
+        titleElement.innerHTML = `<a href="/?shopId=${activeShop.id}" class="shop-title-link">${activeShop.fields.Name} <sup>Shop</sup></a><button id="shop-switcher-trigger" style="background:none; border:none; color:transparent; cursor:pointer; font-size: 1em; vertical-align: super;">s</button>`;
         
         // This listener needs to be attached after the button is created
         document.getElementById('shop-switcher-trigger').addEventListener('click', () => {
