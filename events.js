32b6449f296d90dc36f5dcd5b20830c148be9f04
tylermystery <tyler@tylersmysterytours.com>
2025-11-02 16:49:06 -0800

    Update events.js

--- events.js
+++ events.js
@@ -519,7 +519,7 @@ export function initializeEventListeners(imageCache, flatpickr, shopSettings) {
 
     safeAddEventListener('reset-filters-btn', 'click', () => {
         updateUrl({ category: null, subcategory: null, view: null });
-        const allButton = categoryFiltersContainer?.querySelector('.category-filter-btn[data-filter="all"]');
+        const allButton = categoryFiltersContainer?.querySelector('.category-filter-btn[data-filter=\"all\"]');
         if (allButton) {
             categoryFiltersContainer?.querySelectorAll('.filter-btn').forEach(btn => btn.classList.remove('active'));
             allButton.classList.add('active');
@@ -937,7 +937,7 @@ export function initializeEventListeners(imageCache, flatpickr, shopSettings) {
             triggerSave();
         }
     });
-    const eventPlanDatePicker = flatpickr("#event-date-picker", {
+    const eventPlanDatePicker = flatpickr("#date-filter", {
         dateFormat: "M j, Y",
         onChange: async (selectedDates) => {
             if (state.ui.isInitializing) return;
