bfb001025b28196559034a00bdd79a57900df32c
tylermystery <tyler@tylersmysterytours.com>
2025-11-04 17:54:04 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 import { log } from './utils/debug.js';
 
 export const CONSTANTS = {
-    // ... (all your existing constants, like AUTH_TOKEN_KEY, etc.)
+    // This is your existing constants object
     AUTH_TOKEN_KEY: 'tmt_auth_token',
     DEFAULT_SHOP_ID: 'recZ3H42S4z380T5k',
     DETAIL_TYPES: {
@@ -12,7 +12,6 @@ export const CONSTANTS = {
         GOALS: 'goals'
     },
     FIELD_NAMES: {
-        // ... (all your field names)
         NAME: 'Name',
         DESCRIPTION: 'Description',
         PRICE: 'Price',
@@ -30,7 +29,10 @@ export const CONSTANTS = {
 };
 
 export const STRIPE_PUBLISHABLE_KEY = 'pk_live_51L0C5iHh28kEjv5qjW2tCi3kOOkT9V6fJ1sUqjV8s3j9k9PzGkYkF8sX8c8p2J8sX8c8p2J8sX8c8p2J8';
-
-// --- ADD THESE TWO NEW LINES ---
 export const PUSHER_KEY = '236f480714e5001590b5';
 export const PUSHER_CLUSTER = 'us3';
+
+// --- THIS IS THE FIX ---
+// Add the Cloudinary name as an export so other files can import it.
+export const CLOUDINARY_CLOUD_NAME = 'daedqizre';
+// --- END THE FIX ---
